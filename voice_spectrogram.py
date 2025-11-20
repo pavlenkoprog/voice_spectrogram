fd37e4c142cafd89ed9a5a864910d4ebde47eb2c
pavlenko <pavlenko@tiburon-research.ru>
2025-11-20 21:45:59 +0300

    Настройка разрешения окна спектрограммы

--- voice_spectrogram.py
+++ voice_spectrogram.py
@@ -13,22 +13,32 @@ sample_rate, data = wavfile.read(path)
 if len(data.shape) > 1:
     data = data[:, 0]
 
-# вычисление спектрограммы
+# спектрограмма
 f, t, Sxx = spectrogram(
     data,
     fs=sample_rate,
     window='hann',
-    nperseg=1024,   # длина окна
-    noverlap=512,  # перекрытие
-    scaling='density',
+    nperseg=2048,
+    noverlap=1024,
     mode='magnitude'
 )
 
-# визуализация
+# ограничение частоты до 10 kHz
+freq_limit = 2000
+mask = f <= freq_limit
+
+f = f[mask]
+Sxx = Sxx[mask, :]
+
+# ограничение амплитуды до 150
+Sxx = np.clip(Sxx, 0, 150)
+
+# график
 plt.figure(figsize=(12, 6))
 plt.pcolormesh(t, f, Sxx, shading='gouraud')
 plt.ylabel("Частота (Гц)")
 plt.xlabel("Время (с)")
-plt.title("Спектрограмма (анализатор спектра)")
+plt.title("Спектрограмма (0–2 kHz, амплитуда до 150)")
 plt.colorbar(label="Амплитуда")
+plt.ylim(0, freq_limit)
 plt.show()
